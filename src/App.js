ab9514a1ba1c8e5c1b4e70a9612524a86b0f2bb8
Samantha Schonefeld <samanthaschonefeld@Samis-MacBook-Pro.local>
2019-03-03 22:42:45 -0800

    working on fetch functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import React, { Component } from 'react';
-import Navigation from './Navigation.jsx';
-import Search from './Search.jsx';
-import Header from './Header.jsx'
 import './App.css';
 
 
 class App extends Component {
+  state = {
+    title: '',
+  }
+
+  handleChange(e) {
+    console.log(e.target.value)
+    this.setState({
+      title: e.target.value
+    })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    let encodedTitle = encodeURI(this.state.title)
+    fetch(`https://www.goodreads.com/search.xml?key=zpjgXvUE4HXjFSvSwDTStg&q=${encodedTitle}`)
+    .then((res) => {
+      console.log(res.json())
+      return res.json()
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Navigation />
-        <Search />
+      <div>
+      <input
+        onChange = {(e) => this.handleChange(e)}
+        type = 'text' value = {this.state.zipcode}
+        placeholder = 'zipcode'>
+      </input>
+      <button
+        onClick = {(e) => this.handleSubmit(e)}
+        type = 'submit'>
+        ENTER
+      </button>
       </div>
     );
   }
