1174e60925b6f05790e89459c1637a5803b6a701
Samantha Schonefeld <samanthaschonefeld@Samis-MacBook-Pro.local>
2019-03-05 13:39:51 -0800

    finished app

--- src/About.jsx
+++ src/About.jsx
@@ -3,17 +3,18 @@ import { Link } from 'react-router-dom';
 
 function About(){
   return(
-    <div>
+    <div className = "about">
       <header>
         <h1>ABOUT</h1>
       </header>
       <nav>
       </nav>
       <section>
-      <h2>What's the weather all about?</h2>
-      <p>This app was built using React JS, JSX, and CSS. It gathers weather data by fetching from the <a href = "openweathermap.org/api">OpenWeatherMap</a> API. Thanks for checking it out, hope you enjoyed!</p>
+        <h2>What's the weather all about?</h2>
+        <p className = "about_p">This app was built using React JS, JSX, and CSS. It gathers weather data by fetching from the <a href = "openweathermap.org/api">OpenWeatherMap</a> API. <br/> Thanks for checking it out, hope you enjoyed!</p>
+        <p>You can check out more of my work on my <a href = "https://github.com/samischonefeld">GitHub</a>.</p>
       </section>
-      <li><Link to = '/'>HOME</Link></li>
+      <li className = "home_link"><Link to = '/'>HOME</Link></li>
     </div>
     )
 }
